5bf48907e75dc0d0219aa1b5fbb9a8f00bf28ffa
Sergey Korobok <korvin@kati.ru>
2023-12-18 16:26:18 +0300

    First version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector("#card-template").content; // Темплейт карточки
+const cardTemplateElement = cardTemplate.querySelector(".places__item"); // Элемент внутри тэмплейта
+const cardContainer = document.querySelector(".places__list"); // DOM узел куда добавлять
+
+// функция удаления карточки
+function DeleteCard(evt) {
+  evt.target.closest(".places__item").remove();
+}
+
+// функция добавления новой карточки
+function AddCard(card, deleteFunction) {
+  const cardElement = cardTemplateElement.cloneNode(true); // клонируем шаблон
+  const cardElementImage = cardElement.querySelector(".card__image");
+  const cardElementTitle = cardElement.querySelector(".card__title");
+  const cardElementDelButton = cardElement.querySelector(".card__delete-button");
+  // устанавливаем значения
+  cardElementImage.src = card.link;
+  cardElementImage.alt = card.name;
+  cardElementTitle.textContent = card.name;
+  // добавляем обработчик клика
+  cardElementDelButton.addEventListener("click", deleteFunction);
+  // возврат подготовленного элемента карточки
+  return cardElement;
+}
+
+// выводим все карточки на страницу
+initialCards.forEach( card => {
+  cardContainer.append(AddCard(card, DeleteCard)); // добавляем карточку на страницу
+});
