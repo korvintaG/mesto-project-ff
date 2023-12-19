4f67d4f4e69753ca88aea3f1df69a18a759f919c
Sergey Korobok <korvin@kati.ru>
2023-12-19 11:03:14 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,35 @@ const cardTemplate = document.querySelector("#card-template").content; // Тем
 const cardTemplateElement = cardTemplate.querySelector(".places__item"); // Элемент внутри тэмплейта
 const cardContainer = document.querySelector(".places__list"); // DOM узел куда добавлять
 
-// функция удаления карточки
-function DeleteCard(evt) {
-  evt.target.closest(".places__item").remove();
+/* функция удаления DOM-элемента карточки
+ * @param {DOM-object} cardElement - DOM-элемент, который нужно удалить  
+ */
+function deleteCard(cardElement) {
+  cardElement.remove();
 }
 
-// функция добавления новой карточки
-function AddCard(card, deleteFunction) {
+/* функция создания DOM-элемента новой карточки  
+ * @constructor
+ * @param {object} card - данные о карточке из двух строковых примитивов - имени и ссылки
+ * @param {function} deleteFunction - функция удаления карточки для коллбэка
+ */
+function createCard(cardRecord, deleteFunction) {
   const cardElement = cardTemplateElement.cloneNode(true); // клонируем шаблон
   const cardElementImage = cardElement.querySelector(".card__image"); // изображение
   const cardElementTitle = cardElement.querySelector(".card__title"); // заголовок карточки
   const cardElementDelButton = cardElement.querySelector(".card__delete-button"); // кнопка удаления
   // устанавливаем значения
-  cardElementImage.src = card.link;
-  cardElementImage.alt = card.name;
-  cardElementTitle.textContent = card.name;
+  cardElementImage.src = cardRecord.link;
+  cardElementImage.alt = cardRecord.name;
+  cardElementTitle.textContent = cardRecord.name;
   // добавляем обработчик клика
-  cardElementDelButton.addEventListener("click", deleteFunction);
-  // возврат подготовленного элемента карточки
+  cardElementDelButton.addEventListener("click", () => deleteFunction(cardElement));
+  // возврат подготовленного DOM-элемента карточки
   return cardElement;
 }
 
 // выводим все карточки на страницу
-initialCards.forEach( card => {
-  cardContainer.append(AddCard(card, DeleteCard)); // добавляем карточку на страницу
+initialCards.forEach((cardRecord) => {
+  const cardElement = createCard(cardRecord, deleteCard);
+  cardContainer.append(cardElement); // добавляем карточку на страницу
 });
